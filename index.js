97b69e268d973a3bf3ede986f1bc602cc0292ce5
Gabriel Brun <lydstyl@gmail.com>
2020-11-01 14:47:44 +0100

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const { settings } = require('./settings')
+console.log('settings', settings)
+
+// import removeFolderDuplicates from './removeFolderDuplicates'
+
+// removeFolderDuplicates(origin)
+
+// backUp(origin, backup1)
+
+// backUp(origin, backup2)
+
+// removeFolderDuplicates(backup2)
---
+++ settings.js
@@ -0,0 +1,6 @@
+exports.settings = {
+  origin: './origin',
+
+  backup1: './backup1',
+  backup2: './backup2',
+}
